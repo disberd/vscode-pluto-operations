e516c24a6cdfb889859a9512504298648c2f86e6
Alberto Mengali <disberd@gmail.com>
2022-12-20 09:27:39 +0100

    small refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+// Import the uuid module to generate random UUIDs for the cells
+import * as uuid from 'uuid';
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,28 +13,87 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-pluto-operations" is now active!');
 
+	type CellData = {
+		codeStart: number;
+		codeEnd: number;
+		orderLine: number;
+		hidden: boolean;
+	};
+
+	function newCellData(n: number): CellData {
+		return {
+			codeStart: n,
+			codeEnd: -1,
+			orderLine: -1,
+			hidden: false
+		};
+	}
+
+	function extractStructure(editor: vscode.TextEditor) {
+		const document = editor.document;
+		const cellStructure: { [id: string]: CellData } = {};
+		let tracking = '';
+		let n = 1;
+
+		// Process the first part of the code
+		do {
+			const line = document.lineAt(n);
+			if (line.text.startsWith('# ╔═╡')) {
+				if (uuid.validate(tracking)) {
+					// We have to update the last line of the previous cell
+					cellStructure[tracking].codeEnd = n;
+				}
+				const id = line.text.slice(-36);
+				// Update the tracking
+				tracking = id;
+				cellStructure[id] = newCellData(n)
+			}
+			n++;
+		} while (tracking !== '# ╔═╡ Cell order:');
+		// Now we just parse the part with the cell order
+		while (n < document.lineCount) {
+			const line = document.lineAt(n);
+			const id = line.text.slice(-36);
+			const data = cellStructure[id];
+			data.hidden = line.text.startsWith('# ╟─') ? false : true;
+			data.orderLine = n;
+			n++;
+		}
+		return cellStructure;
+	}
+
 	function showCellId() {
 		const editor = vscode.window.activeTextEditor;
 		if (editor) {
-			const id = currentCellId(editor);
-			if (id) {
-				vscode.window.showInformationMessage(id);
-			}
+			const out = extractStructure(editor);
+			vscode.window.showInformationMessage(JSON.stringify(out));
 		}
 	}
 
 	function currentCellId(editor: vscode.TextEditor) {
-		// Get the active text editor
-		if (editor) {
-			const document = editor.document;
-			const currentPosition = editor.selection.active;
+		const document = editor.document;
+		const currentPosition = editor.selection.active;
 
-			for (let n = currentPosition.line; n > 0; n--) {
-				const line = document.lineAt(n);
-				if (line.text.startsWith('# ╔═╡')) {
-					const id = line.text.slice(-36);
-					return id;
-				}
+		for (let n = currentPosition.line; n > 0; n--) {
+			const line = document.lineAt(n);
+			if (line.text.startsWith('# ╔═╡')) {
+				const id = line.text.slice(-36);
+				return { id, line: n };
+			}
+		}
+	}
+
+	function getCellOrder(editor: vscode.TextEditor) {
+		const document = editor.document;
+		const order = [];
+		for (let n = document.lineCount - 1; n > 0; n--) {
+			const line = document.lineAt(n);
+			if (line.text.startsWith('# ╔═╡ Cell order:')) {
+				return order;
+			} else {
+				const hidden = line.text.startsWith('# ╟─') ? false : true;
+				const id = line.text.slice(-36);
+				order.unshift({ id, hidden });
 			}
 		}
 	}
