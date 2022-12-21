d5f39b9222b5d52987ef8f7a385a5d48eacf7b7d
Alberto Mengali <disberd@gmail.com>
2022-12-21 16:10:49 +0100

    add cell commands

--- src/extension.ts
+++ src/extension.ts
@@ -13,6 +13,17 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-pluto-operations" is now active!');
 
+	const _mainfestId = '00000000-0000-0000-0000-000000000002';
+	const _cellOrderString = '# ╔═╡ Cell order:';
+	const _cellDelimiter = '# ╔═╡ ';
+	const _hiddenCellDelimiter = '# ╟─';
+	const _shownCellDelimiter = '# ╠═';
+
+	function isCellLine(linetext: string) {
+		const isCell = [_cellDelimiter, _hiddenCellDelimiter, _shownCellDelimiter].some(s => linetext.startsWith(s));
+		return isCell;
+	}
+
 	type CellData = {
 		codeStart: number;
 		codeEnd: number;
@@ -33,40 +44,97 @@ export function activate(context: vscode.ExtensionContext) {
 		const document = editor.document;
 		const cellStructure: { [id: string]: CellData } = {};
 		let tracking = '';
-		let n = 1;
-
+		let n = 0;
 		// Process the first part of the code
-		do {
+		for (n; n < document.lineCount; n++) {
 			const line = document.lineAt(n);
-			if (line.text.startsWith('# ╔═╡')) {
-				if (uuid.validate(tracking)) {
+			if (line.text.startsWith(_cellDelimiter)) {
+				if (cellStructure[tracking]) {
 					// We have to update the last line of the previous cell
-					cellStructure[tracking].codeEnd = n;
+					cellStructure[tracking].codeEnd = n-1;
 				}
 				const id = line.text.slice(-36);
 				// Update the tracking
 				tracking = id;
-				cellStructure[id] = newCellData(n)
+				cellStructure[id] = newCellData(n);
 			}
-			n++;
-		} while (tracking !== '# ╔═╡ Cell order:');
-		// Now we just parse the part with the cell order
-		while (n < document.lineCount) {
+			if (tracking === _mainfestId) {
+				// We stop when we find the manifest cell as it's the last one
+				break;
+			}
+		}  
+		// Now we just parse the last part of the notebook with the cell order
+		for (n = document.lineCount - 1; n > 0; n--) {
 			const line = document.lineAt(n);
-			const id = line.text.slice(-36);
-			const data = cellStructure[id];
-			data.hidden = line.text.startsWith('# ╟─') ? false : true;
-			data.orderLine = n;
-			n++;
+			if (line.text === _cellOrderString) {
+				// We reached the start of the cell order. We update the mainfest cell data to also save the last code line
+				cellStructure[_mainfestId].codeEnd = n-1;
+				break;
+			} else {
+				const text = line.text;
+				if (!isCellLine(text)) {continue;}; // We skip cells which are not valid cell identifiers
+				// We just parse the cell id and update order and hidden status
+				const id = text.slice(-36);
+				const data = cellStructure[id];
+				data.hidden = text.startsWith(_hiddenCellDelimiter) ? true : false;
+				data.orderLine = n;
+			}
 		}
 		return cellStructure;
+	}
+
+	function removeCell(editor: vscode.TextEditor) {
+		const cellStructure = extractStructure(editor);
+		const id = currentCellId(editor);
+		if (id === '') {
+			vscode.window.showErrorMessage('You are not currently over a valid cell');
+		}
+		// editor.edit(editBuilder => {
+		// 	const currentCell = cellStructure[id];
+		// 	editBuilder.delete(new vscode.Range(currentCell.codeStart, 0, currentCell.codeEnd+1))
+		// 	// Insert the cell in the order
+		// 	editBuilder.insert(newOrderPosition, _shownCellDelimiter + newId + '\n');
+		// 	// Insert the cell in the code
+		// 	editBuilder.insert(newCodePosition, _cellDelimiter + newId + '\n\n');
+		// });
+	}
+	function addCell(editor: vscode.TextEditor, after: boolean = true) {
+		const cellStructure = extractStructure(editor);
+		const id = currentCellId(editor);
+		if (id === '') {
+			vscode.window.showErrorMessage('You are not currently over a valid cell');
+		}
+		const newId = uuid.v4();
+		editor.edit(editBuilder => {
+			const currentCell = cellStructure[id];
+			const newCodePosition = new vscode.Position(after ? currentCell.codeEnd + 1 : currentCell.codeStart, 0);
+			const newOrderPosition = new vscode.Position(currentCell.orderLine + (after ? 1 : 0), 0);
+			// Insert the cell in the order
+			editBuilder.insert(newOrderPosition, _shownCellDelimiter + newId + '\n');
+			// Insert the cell in the code
+			editBuilder.insert(newCodePosition, _cellDelimiter + newId + '\n\n');
+		});
+	}
+
+	function addCellAfter() {
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			addCell(editor, true);
+		}
+	}
+
+	function addCellBefore() {
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			addCell(editor, false);
+		}
 	}
 
 	function showCellId() {
 		const editor = vscode.window.activeTextEditor;
 		if (editor) {
-			const out = extractStructure(editor);
-			vscode.window.showInformationMessage(JSON.stringify(out));
+			const out = currentCellId(editor);
+			vscode.window.showInformationMessage(out);
 		}
 	}
 
@@ -76,33 +144,21 @@ export function activate(context: vscode.ExtensionContext) {
 
 		for (let n = currentPosition.line; n > 0; n--) {
 			const line = document.lineAt(n);
-			if (line.text.startsWith('# ╔═╡')) {
-				const id = line.text.slice(-36);
-				return { id, line: n };
+			const text = line.text;
+			if (isCellLine(text)) {
+				const id = text.slice(-36);
+				return id;
 			}
 		}
+		return '';
 	}
 
-	function getCellOrder(editor: vscode.TextEditor) {
-		const document = editor.document;
-		const order = [];
-		for (let n = document.lineCount - 1; n > 0; n--) {
-			const line = document.lineAt(n);
-			if (line.text.startsWith('# ╔═╡ Cell order:')) {
-				return order;
-			} else {
-				const hidden = line.text.startsWith('# ╟─') ? false : true;
-				const id = line.text.slice(-36);
-				order.unshift({ id, hidden });
-			}
-		}
-	}
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-pluto-operations.current_cell_id', showCellId);
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('vscode-pluto-operations.current_cell_id', showCellId));
+	context.subscriptions.push(vscode.commands.registerCommand('vscode-pluto-operations.add_cell_after', addCellAfter));
+	context.subscriptions.push(vscode.commands.registerCommand('vscode-pluto-operations.add_cell_before', addCellBefore));
 }
 
 // This method is called when your extension is deactivated
